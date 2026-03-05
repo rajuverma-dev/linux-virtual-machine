5d4f88760c6488864a76a99051815b32c31db191
rajuverma-dev <rajuverma.dev@gmail.com>
2026-03-05 21:12:56 +0530

    added code @5Mar26#9:12PM

---
+++ main.tf
@@ -0,0 +1,54 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = var.rg_location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  address_space       = var.address_space
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "snet" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.address_prefixes
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = var.nic_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.snet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = var.vm_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_F2"
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,43 @@
+variable "rg_name" {
+  type    = string
+  default = "veer-vm-rg"
+}
+
+variable "rg_location" {
+  type    = string
+  default = "Norway East"
+}
+
+variable "vnet_name" {
+  type    = string
+  default = "veer-vnet"
+}
+
+variable "address_space" {
+  type    = list(string)
+  default = ["10.0.0.0/16"]
+}
+
+variable "address_prefixes" {
+  type    = list(string)
+  default = ["10.0.2.0/24"]
+}
+
+variable "nic_name" {
+  type    = string
+  default = "veer-nic"
+}
+
+variable "vm_name" {
+  type    = string
+  default = "veer-vm"
+}
+
+variable "admin_username" {
+  type    = string
+  default = "adminuser"
+}
+
+variable "admin_password" {
+  type = string
+}
